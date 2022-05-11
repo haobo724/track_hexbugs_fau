c118b123d0cace17788b120a20715b8dd9b14d2d
haobo724 <s724724@gmail.com>
2022-05-11 12:47:48 +0200

    merge_mask_input_as_video

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 import os
 import glob
-from tools import sort_humanly,delet_contours
+from tools import sort_humanly, delet_contours
+
+
 def crop_face(image, face_rect):
     top = max(face_rect[1], 0)
     left = max(face_rect[0], 0)
@@ -11,10 +13,11 @@ def crop_face(image, face_rect):
     return image[top:bottom, left:right, :]
 
 
-def get_bb(img,mask):
+def get_bb(img, mask):
     ret, thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # 大津阈值
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # cv2.RETR_EXTERNAL 定义只检测外围轮廓
-    delete_list=[]
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)  # cv2.RETR_EXTERNAL 定义只检测外围轮廓
+    delete_list = []
     if len(contours) == 0:
         cv2.putText(img, 'not found object', (100, 100), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 1)
     else:
@@ -22,7 +25,7 @@ def get_bb(img,mask):
         for i in range(len(contours)):
             l = len(contours[i])
             area = cv2.contourArea(contours[i])
-            if cv2.contourArea(contours[i]) <100:
+            if cv2.contourArea(contours[i]) < 100:
                 delete_list.append(i)
             # delet contour 是序号
         contours = delet_contours(contours, delete_list)
@@ -40,53 +43,54 @@ def get_bb(img,mask):
             mu = cv2.moments(box, False)
             try:
                 mc = [mu['m10'] / mu['m00'], mu['m01'] / mu['m00']]
-                cv2.drawMarker(img, (int(mc[0]),int(mc[1])),  (0, 0, 255), thickness=2)
-            except :
+                cv2.drawMarker(img, (int(mc[0]), int(mc[1])), (0, 0, 255), thickness=2)
+            except:
                 print('no b')
     # img_new = crop_face(img, newregion)
     return img
-    # cv2.imshow('a', img)
-    # cv2.waitKey()
-    # cv2.imshow('b', img_new)
-    # cv2.waitKey()
-
-if __name__ == '__main__':
-    img_dirs = sort_humanly(os.listdir('output'))
-    mask_dirs = sort_humanly(os.listdir('labeled\output'))
-    for img_dir ,mask_dir in zip(img_dirs,mask_dirs):
-        
 
 
-        imgs =sort_humanly(glob.glob(f'output/{img_dir}/*.jpg'))
-        masks =sort_humanly(glob.glob(f'labeled\output\{mask_dir}/*.jpg'))
+def merge_mask_input_as_video():
+    path_to_img_dirs= 'output'
+    path_to_mask_dirs= 'labeled\output'
+    if not os.path.exists(path_to_img_dirs) or not os.path.exists(path_to_mask_dirs):
+        raise FileNotFoundError('no dirs')
+
+    img_dirs = sort_humanly(os.listdir(path_to_img_dirs))
+    mask_dirs = sort_humanly(os.listdir(path_to_mask_dirs))
+    for img_dir, mask_dir in zip(img_dirs, mask_dirs):
+
+        imgs = sort_humanly(glob.glob(f'{path_to_img_dirs}/{img_dir}/*.jpg'))
+        masks = sort_humanly(glob.glob(f'{path_to_mask_dirs}\{mask_dir}/*.jpg'))
 
         codec = cv2.VideoWriter_fourcc(*'mp4v')
-        frameSize_s=cv2.imread(imgs[0]).shape[:2]
-        rotation_flag  = False
+        frameSize_s = cv2.imread(imgs[0]).shape[:2]
+        rotation_flag = False
 
-        if frameSize_s[0]/frameSize_s[1]>1: #(768, 1024)
+        if frameSize_s[0] / frameSize_s[1] > 1:  # (768, 1024)
             print('rotation start')
-            rotation_flag =True
+            rotation_flag = True
             # name = os.path.split(img_dir)[-1]+ '_rotated.mp4'
-        else:
-            print(img_dir, mask_dir,'skiped')
-            continue
-        name = os.path.split(img_dir)[-1]+ '.mp4'
+        # else:
+        #     print(img_dir, mask_dir, 'skiped')
+        #     continue
+        name = os.path.split(img_dir)[-1] + '.mp4'
         # input()
         out_path = os.path.join('./video_output/', name)
-        print(img_dir,mask_dir)
+        print(img_dir, mask_dir)
         out = cv2.VideoWriter(out_path, codec, 5, frameSize_s[::-1])
-    
-    
-    
-        for i , m in zip(imgs, masks):
+
+        for i, m in zip(imgs, masks):
             img = cv2.imread(i)
             mask = cv2.imread(m, 0)
 
             if rotation_flag:
-                mask=np.rot90(mask, k=-1)
-                # img = cv2.imread(i)
+                mask = np.rot90(mask, k=-1)
 
-            concat=get_bb(img,mask)
+            concat = get_bb(img, mask)
             out.write(concat)
         out.release()
+
+
+if __name__ == '__main__':
+  merge_mask_input_as_video()
